1fe527b019fa6a1da03f6e6c0665e941fbcc05e5
SomenathPaul <somenathpaulbusiness10@gmail.com>
2025-09-20 19:13:15 +0530

    update polymorphism

---
+++ Polymorphism/Type2_Runtime_Polymorphism/RuntimePolymorphism.java
@@ -0,0 +1,31 @@
+package Polymorphism.Type2_Runtime_Polymorphism;
+
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class RuntimePolymorphism {
+    public static void main(String[] args) {
+        Animal a1 = new Dog();  // upcasting
+        Animal a2 = new Cat();
+        
+        a1.sound();  // Dog barks
+        a2.sound();  // Cat meows
+    }
+}
